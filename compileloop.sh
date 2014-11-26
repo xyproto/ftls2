c07ac59727767cc540e007ceb1d742d652aa3c4d
Alexander F Rødseth <rodseth@gmail.com>
2014-11-26 16:17:41 +0100

    Only checking local sources. + hashing with bcrypt

--- compileloop.sh
+++ compileloop.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SOURCE=
-for f in *.go ../onthefly/*.go ../genericsite/*.go ../instapage/*.go ../simpleredis/*.go ../siteengines/*.go ../personplan/*.go ../moskus/*.go ../webhandle/*.go; do
+for f in *.go; do
+#../onthefly/*.go ../genericsite/*.go ../instapage/*.go ../simpleredis/*.go ../siteengines/*.go ../personplan/*.go ../moskus/*.go ../webhandle/*.go; do
   SOURCE+="$f "
 done
 BIN=ftlsweb
